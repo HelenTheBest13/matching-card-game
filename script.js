0bced4797f38544e144e312d5ffbaf79af9316a9
HelenTheBest13 <helen4icodestudent@outlook.com>
2025-05-22 16:16:47 -0500

    finished

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
 const cards = document.querySelectorAll('.card');
 const restartButton = document.getElementById('restart');
-let firstcard = null;
-let secondcard = null;
-let lockcard = false;
-function shuffleCards() {
-cards.foreach(card => {
-let randompos = Math.floor(Math.random() * 12);
-card. style.order = randompos;
-});
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false; 
+function shuffleCards () {
+     cards.forEach(card =>  {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+     });    
 }
+function flipCard() {
+    if (lockBoard) return;
+    if (this=== firstCard) return; 
+
+    this.classList.add('flipped');
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+
+cards.forEach(card => {
+    const value = card.dataset.value;
+    const img = document.createElement('img');
+    img.src = `images/${value}.png`;
+    card.appendChild(img);
+    card.addEventListener('click', flipCard);
+});
+
+
+shuffleCards();
+
+function checkForMatch() {
+    if (firstCard.dataset.value === secondCard.dataset.value) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+function unflipCards() {
+    lockBoard = true;
+setTimeout(() => {
+    firstCard.classList.remove('flipped');
+    secondCard.classList.remove('flipped');
+    resetBoard();
+}, 1000)
+}
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+restartButton.addEventListener('click', () => {
+    cards.forEach(card => {
+        card.classList.remove('flipped');
+        card.addEventListener('click', flipCard);
+    });
+    shuffleCards();
+});
